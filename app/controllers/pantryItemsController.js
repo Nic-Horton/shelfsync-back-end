746998a8c494413a77fbb4b90351bd84e2f9cb13
Nicolas Horton <nic.horton00@gmail.com>
2024-02-28 16:15:39 -0600

    Created routs and controllers for necessary functions the use will need when managing inventory of their items

--- app/controllers/pantryItemsController.js
+++ app/controllers/pantryItemsController.js
@@ -1,13 +1,127 @@
 const PantryItem = require('../models').PantryItem;
 
-exports.show = (request, response) => {
-	return PantryItem.findByPk(request.params.pantryItemId, {})
-		.then((pantryItem) => {
-			if (!pantryItem) {
-				response.status(404).send({ error: 'Item not found' });
+exports.show = async (request, response) => {
+	return await PantryItem.findAll({ where: { userID: request.userId } }, {})
+		.then((pantryItems) => {
+			if (!pantryItems) {
+				response.status(404).send({ error: 'No items not found' });
 			} else {
-				response.status(200).send(pantryItem);
+				response.status(200).send(pantryItems);
 			}
 		})
 		.catch((error) => response.status(400).send(error));
+};
+
+exports.create = async (request, response) => {
+	return await PantryItem.create(
+		{
+			userID: request.userId,
+			name: request.body.name,
+			quantity: request.body.quantity,
+			unit: request.body.unit,
+			category: request.body.category,
+		},
+		{}
+	).then((newPantryItem) =>
+		PantryItem.findByPk(newPantryItem.id, {})
+			.then((newPantryItem) => response.status(201).send(newPantryItem))
+			.catch((error) => response.status(400).send(error))
+	);
+};
+
+exports.delete = async (request, response) => {
+	try {
+		const { pantryItemId } = request.params;
+		const pantryItem = await PantryItem.findByPk(pantryItemId);
+
+		if (!pantryItem) {
+			return response.status(404).send('Item not found');
+		}
+
+		await pantryItem.destroy();
+		return response.status(204).send('Item deleted');
+	} catch (error) {
+		return response
+			.status(500)
+			.send('Error deleting pantry item: ' + error.message);
+	}
+};
+
+exports.update = async (request, response) => {
+	try {
+		const { pantryItemId } = request.params;
+		const { name, quantity, unit, category } = request.body;
+		const updateFields = {};
+		const pantryItem = await PantryItem.findByPk(pantryItemId);
+
+		if (!pantryItem) {
+			return response.status(404).send('Pantry item not found');
+		}
+
+		if (name !== undefined && name !== pantryItem.name) {
+			// pantryItem.name = request.body.name;
+			updateFields.name = name;
+		}
+		if (quantity !== undefined && quantity !== pantryItem.quantity) {
+			// pantryItem.quantity = request.body.quantity;
+			updateFields.quantity = quantity;
+		}
+		if (unit !== undefined && unit !== pantryItem.unit) {
+			// pantryItem.unit = request.body.unit;
+			updateFields.unit = unit;
+		}
+		if (category !== undefined && category !== pantryItem.category) {
+			// pantryItem.category = request.body.category;
+			updateFields.category = category;
+		}
+
+		console.log(updateFields);
+		if (Object.keys(updateFields).length === 0) {
+			return response.status(300).send('Item record has nothing to update');
+		}
+
+		await pantryItem.update(updateFields);
+
+		return response.status(200).send(pantryItem);
+	} catch (error) {
+		return response
+			.status(500)
+			.send('Error updating pantry item: ' + error.message);
+	}
+};
+
+exports.search = async (request, response) => {
+	const { search } = request.query;
+
+	try {
+		const pantryItems = await PantryItem.findAll({
+			where: {
+				[Op.and]: [
+					{ userID: request.userId },
+					{
+						[Op.or]: [
+							{
+								name: {
+									[Op.iLike]: '%' + search + '%',
+								},
+							},
+							{
+								category: {
+									[Op.iLike]: '%' + search + '%',
+								},
+							},
+						],
+					},
+				],
+			},
+		});
+
+		if (pantryItems.length === 0) {
+			return response.status(404).send('No items found');
+		}
+
+		return response.json(pantryItems);
+	} catch (error) {
+		response.status(500).send('Error searching pantry items: ' + error.message);
+	}
 };
--- app/routes/pantryItemRoutes.js
+++ app/routes/pantryItemRoutes.js
@@ -1,5 +1,30 @@
+const { authorizeJwt } = require('../middleware');
 const pantryItemsController = require('../controllers').pantryItems;
 
 module.exports = (app) => {
-	app.get('/pantryItems/:pantryItemId', pantryItemsController.show);
+	app.get(
+		'/pantryItems',
+		[authorizeJwt.verifyToken],
+		pantryItemsController.show
+	);
+	app.post(
+		'/pantryItems',
+		[authorizeJwt.verifyToken],
+		pantryItemsController.create
+	);
+	app.delete(
+		'/pantryItems/:pantryItemId',
+		[authorizeJwt.verifyToken],
+		pantryItemsController.delete
+	);
+	app.put(
+		'/pantryItems/:pantryItemId',
+		[authorizeJwt.verifyToken],
+		pantryItemsController.update
+	);
+	app.get(
+		'/pantryItems/search',
+		[authorizeJwt.verifyToken],
+		pantryItemsController.search
+	);
 };
